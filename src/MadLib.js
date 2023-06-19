4b63839844e60e3d784d7043a9d2851e9f920dea
Anthony Ramos <ramosanthony@gmail.com>
2023-06-19 14:00:49 -0400

    Added MadLib Compopnent and App Smoke Test

---
+++ src/MadLib.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MadLib = () => {
+  return (
+  <div>MadLibs!</div>
+  )
+}
+
+export default MadLib;
---
+++ src/MadLib.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import MadLib from './MadLib';
+
+it("will render without crashing", () => {
+  render(<MadLib />);
+});
